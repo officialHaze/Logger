2480ecccbf93ad20f7c108334b26bdfd02be51f6
Assassin <moinakdey.166@outlook.com>
2023-08-15 20:14:00 +0530

    Minor changes

--- Src/index.ts
+++ Src/index.ts
@@ -9,23 +9,29 @@ interface LoggerOptions
 
 class Logger
 {
-    environment: string = "development"
-    isReact: boolean = false
+    static defaultEnvironment: string;
+    static defaultIsReact: boolean;
+
+    constructor(loggerOptions: LoggerOptions)
+    {
+        const {environment, isReact} = loggerOptions
+        Logger.defaultEnvironment = environment,
+        Logger.defaultIsReact = isReact
+    }
 
     create(loggerOptions: LoggerOptions)
     {
-        const {environment, isReact} = loggerOptions
-        this.environment = environment,
-        this.isReact = isReact
+        const logger = new Logger(loggerOptions)
+        return logger
     }
 
-    log(message: string, env: string | null | undefined, isreact: boolean | null | undefined)
+    log(message: string, env?: string, isreact?: boolean)
     {
-        const isReactPlatform = isreact ? isreact : this.isReact
-        const projenv = isReactPlatform ? process.env.REACT_APP_LOGGER_ENVIRONMENT : process.env.LOGGER_ENVIRONMENT
-        const userDefinedEnv = env ? env : this.environment
+        const isPlatformReact = isreact!==undefined ? isreact : Logger.defaultIsReact
+        const projenv = isPlatformReact ? process.env.REACT_APP_LOGGER_ENVIRONMENT : process.env.LOGGER_ENVIRONMENT
+        const userDefinedEnv = env ? env : Logger.defaultEnvironment
 
-        const projenv_ = projenv ? projenv : this.environment
+        const projenv_ = projenv ? projenv : Logger.defaultEnvironment
 
         if(projenv_ === userDefinedEnv)
         {
@@ -34,7 +40,6 @@ class Logger
     }
 }
 
-const logger = new Logger()
+const logger = new Logger({environment: "development", isReact: false})
 
 export default logger
-
